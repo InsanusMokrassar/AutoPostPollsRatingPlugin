4abb3a6aadb85557cc8a93ab727219af028ed245
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-05-22 14:50:59 +0600

    0.6.0

--- src/main/kotlin/com/github/insanusmokrassar/AutoPostPollsRatingPlugin/RatingsVariants.kt
+++ src/main/kotlin/com/github/insanusmokrassar/AutoPostPollsRatingPlugin/RatingsVariants.kt
@@ -3,12 +3,14 @@ package com.github.insanusmokrassar.AutoPostPollsRatingPlugin
 import com.github.insanusmokrassar.AutoPostTelegramBot.base.plugins.abstractions.Rating
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.internal.*
 
 typealias RatingsVariants = Map<String, Rating>
 
 @Serializer(Map::class)
-object RatingsVariantsSerializer : KSerializer<RatingsVariants> by LinkedHashMapSerializer(
-    StringSerializer,
-    FloatSerializer
+object RatingsVariantsSerializer : KSerializer<RatingsVariants> by MapSerializer(
+    String.serializer(),
+    Float.serializer()
 )
